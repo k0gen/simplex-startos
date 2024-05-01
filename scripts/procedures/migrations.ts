d50a15339882a98d61f922c33ef88d1f2da81be1
Mariusz Kogen <k0gen@pm.me>
2024-05-01 10:48:19 +0200

    Add migration and update workflows

--- scripts/procedures/migrations.ts
+++ scripts/procedures/migrations.ts
@@ -1,4 +1,28 @@
 import { compat, types as T } from "../deps.ts";
 
-export const migration: T.ExpectedExports.migration = compat.migrations
-    .fromMapping({}, "5.6.2" );
+export const migration: T.ExpectedExports.migration =
+  compat.migrations.fromMapping(
+    {
+      "5.6.2": {
+        up: compat.migrations.updateConfig(
+          (config: any) => {
+            config["xftp-address"];
+
+            return config;
+          },
+          true,
+          { version: "5.6.2", type: "up" }
+        ),
+        down: compat.migrations.updateConfig(
+          (config: any) => {
+            delete config["xftp-address"];
+
+            return config;
+          },
+          true,
+          { version: "5.6.2", type: "down" }
+        ),
+      },
+    },
+    "5.6.2"
+  );
